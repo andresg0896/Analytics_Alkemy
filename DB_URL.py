86a0a0e662e90c744c024a5c044e5fcbf7e3ac4e
AndresG <74727391+andresg0896@users.noreply.github.com>
2022-07-28 18:42:28 -0500

    Update DB_URL.py

--- DB_URL.py
+++ DB_URL.py
@@ -3,7 +3,7 @@ url_museo = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca
 url_cine = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv'
 url_biblioteca = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
 # Se asignan los datos de conexión. Modificar aquí.
-pg_usuario= 'postgres'
+pg_usuario = 'postgres'
 pg_clave = ''
 pg_host = 'localhost'
 pg_puerto = '5432'
